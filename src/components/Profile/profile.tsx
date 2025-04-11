13c99eae16192a3f9aa12ad549abf2ce9c172444
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-11 16:38:27 +0530

    Fix casing in import statement for profile.css

--- src/components/Profile/profile.tsx
+++ src/components/Profile/profile.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import portfolioData from '../../data/portfolioData';
-import './Profile.css';
+import './profile.css';
 
 const Profile: React.FC = () => {
   const { name, title, socialLinks } = portfolioData;
